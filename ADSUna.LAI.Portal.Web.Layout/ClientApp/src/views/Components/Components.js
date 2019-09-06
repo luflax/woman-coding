217295423759a40f54b09c8c3cdef16e96fb5418
Gercy Campos <gercy@outlook.com.br>
2019-09-06 10:15:59 -0300

    Correção do build da pagina react

--- ADSUna.LAI.Portal.Web.Layout/ClientApp/src/views/Components/Components.js
+++ ADSUna.LAI.Portal.Web.Layout/ClientApp/src/views/Components/Components.js
@@ -7,28 +7,28 @@ import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
 // core components
-import Header from "../../components/Header/Header.js";
-import Footer from "/components/Footer/Footer.js";
-import GridContainer from "/components/Grid/GridContainer.js";
-import GridItem from "/components/Grid/GridItem.js";
-import Button from "/components/CustomButtons/Button.js";
-import Parallax from "/components/Parallax/Parallax.js";
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import Parallax from "components/Parallax/Parallax.js";
 // sections for this page
-import HeaderLinks from "/components/Header/HeaderLinks.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
 import SectionBasics from "./Sections/SectionBasics.js";
-import SectionNavbars from "/Sections/SectionNavbars.js";
-import SectionTabs from "/Sections/SectionTabs.js";
-import SectionPills from "/Sections/SectionPills.js";
-import SectionNotifications from "/Sections/SectionNotifications.js";
-import SectionTypography from "/Sections/SectionTypography.js";
-import SectionJavascript from "/Sections/SectionJavascript.js";
+import SectionNavbars from "./Sections/SectionNavbars.js";
+import SectionTabs from "./Sections/SectionTabs.js";
+import SectionPills from "./Sections/SectionPills.js";
+import SectionNotifications from "./Sections/SectionNotifications.js";
+import SectionTypography from "./Sections/SectionTypography.js";
+import SectionJavascript from "./Sections/SectionJavascript.js";
 import SectionCarousel from "./Sections/SectionCarousel.js";
-import SectionCompletedExamples from "/Sections/SectionCompletedExamples.js";
-import SectionLogin from "/Sections/SectionLogin.js";
-import SectionExamples from "/Sections/SectionExamples.js";
-import SectionDownload from "/Sections/SectionDownload.js";
+import SectionCompletedExamples from "./Sections/SectionCompletedExamples.js";
+import SectionLogin from "./Sections/SectionLogin.js";
+import SectionExamples from "./Sections/SectionExamples.js";
+import SectionDownload from "./Sections/SectionDownload.js";
 
-import styles from "/assets/jss/material-kit-react/views/components.js";
+import styles from "assets/jss/material-kit-react/views/components.js";
 
 const useStyles = makeStyles(styles);
 
@@ -38,7 +38,7 @@ export default function Components(props) {
   return (
     <div>
       <Header
-        brand="Material Kit React"
+        brand="Bichos Gerais"
         rightLinks={<HeaderLinks />}
         fixed
         color="transparent"
@@ -48,7 +48,7 @@ export default function Components(props) {
         }}
         {...rest}
       />
-      <Parallax image={require("/assets/img/bg4.jpg")}>
+      <Parallax image={require("assets/img/bg4.jpg")}>
         <div className={classes.container}>
           <GridContainer>
             <GridItem>
